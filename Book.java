f2ec45ee463e948909fcdbde5400c300d1d06745
Elvis <elvis@sendyit.com>
2023-03-17 18:45:27 +0300

    Initial commit

---
+++ Book.java
@@ -0,0 +1,75 @@
+public class Book {
+    private String title;
+    private String author;
+    private String datePublished;
+    private int numberOfPages;
+    private String edition;
+    private String genre;
+    private String description;
+
+    public Book(String title, String author, String datePublished, int numberOfPages, String edition, String genre, String description) {
+        this.title = title;
+        this.author = author;
+        this.datePublished = datePublished;
+        this.numberOfPages = numberOfPages;
+        this.edition = edition;
+        this.genre = genre;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDatePublished() {
+        return datePublished;
+    }
+
+    public void setDatePublished(String datePublished) {
+        this.datePublished = datePublished;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
